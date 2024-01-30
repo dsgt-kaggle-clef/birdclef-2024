e798fcee7d474ed3291790806d28a08c9d1de7d3
Murilo Gustineli <murilogustineli@gmail.com>
2024-01-30 16:25:46 +0000

    added birdclef folder with utils.py file

---
+++ birdclef/utils.py
@@ -0,0 +1,31 @@
+import os
+import sys
+from contextlib import contextmanager
+
+from pyspark.sql import SparkSession
+
+os.environ["PYSPARK_PYTHON"] = sys.executable
+os.environ["PYSPARK_DRIVER_PYTHON"] = sys.executable
+
+
+def get_spark(cores=4, memory="8g"):
+    """Get a spark session for a single driver."""
+    return (
+        SparkSession.builder.config("spark.driver.memory", memory)
+        .config("spark.driver.cores", cores)
+        .config("spark.sql.execution.arrow.pyspark.enabled", "true")
+        .config("spark.driver.maxResultSize", "4g")
+        .getOrCreate()
+    )
+
+
+@contextmanager
+def spark_resource(*args, **kwargs):
+    """A context manager for a spark session."""
+    spark = None
+    try:
+        spark = get_spark(*args, **kwargs)
+        yield spark
+    finally:
+        if spark is not None:
+            spark.stop()
