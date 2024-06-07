bbe89008cab8b5dced6b73212de7492abb2b930f
Murilo Gustineli <murilogustineli@gmail.com>
2024-06-07 15:19:00 +0000

    updated model with BCEWithLogitsLoss and MultilabelAUROC metric

--- birdclef/experiment/google_vocalization/model.py
+++ birdclef/experiment/google_vocalization/model.py
@@ -1,32 +1,22 @@
 import pytorch_lightning as pl
 import torch
 from torch import nn
-from torchmetrics.classification import (
-    MulticlassAccuracy,
-    MulticlassF1Score,
-    MulticlassPrecision,
-    MulticlassRecall,
-)
+from torchmetrics.classification import MultilabelAUROC
 
 
 class LinearClassifier(pl.LightningModule):
-    def __init__(self, num_features: int, num_classes: int, asl_loss: bool = False):
+    def __init__(self, num_features: int, num_classes: int):
         super().__init__()
         self.num_features = num_features
         self.num_classes = num_classes
         self.save_hyperparameters()  # Saves hyperparams in the checkpoints
-        self.loss = torch.nn.functional.nll_loss
+        self.loss = nn.BCEWithLogitsLoss()
         self.model = nn.Linear(num_features, num_classes)
         self.learning_rate = 0.002
-        self.accuracy = MulticlassAccuracy(num_classes=num_classes, average="weighted")
-        self.f1_score = MulticlassF1Score(num_classes=num_classes, average="weighted")
-        self.precision = MulticlassPrecision(
-            num_classes=num_classes, average="weighted"
-        )
-        self.recall = MulticlassRecall(num_classes=num_classes, average="weighted")
+        self.auroc_score = MultilabelAUROC(num_classes=num_classes, average="weighted")
 
     def forward(self, x):
-        return torch.log_softmax(self.model(x), dim=1)
+        return self.model(x)
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
@@ -38,30 +28,11 @@ class LinearClassifier(pl.LightningModule):
         loss = self.loss(logits, y)
         self.log(f"{step_name}_loss", loss, prog_bar=True)
         self.log(
-            f"{step_name}_accuracy",
-            self.accuracy(logits, y),
+            f"{step_name}_auroc",
+            self.auroc_score(logits, y),
             on_step=False,
             on_epoch=True,
         )
-        if step_name != "train":
-            self.log(
-                f"{step_name}_f1",
-                self.f1_score(logits, y),
-                on_step=False,
-                on_epoch=True,
-            )
-            self.log(
-                f"{step_name}_precision",
-                self.precision(logits, y),
-                on_step=False,
-                on_epoch=True,
-            )
-            self.log(
-                f"{step_name}_recall",
-                self.recall(logits, y),
-                on_step=False,
-                on_epoch=True,
-            )
         return loss
 
     def training_step(self, batch, batch_idx):
