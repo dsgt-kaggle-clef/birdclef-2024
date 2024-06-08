ce1bf207252959522dfb2ce75856956ceefa66e6
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2024-06-08 01:24:36 +0100

    Add updated tests for make_submission

--- tests/test_label_data.py
+++ tests/test_label_data.py
@@ -1,54 +1,7 @@
-import numpy as np
-import pandas as pd
-import pytest
-import torch
-import torchaudio
-
 from birdclef.label.data import (
     GoogleVocalizationSoundscapeDataModule,
     GoogleVocalizationSoundscapeDataset,
 )
-
-
-@pytest.fixture
-def metadata_path(tmp_path):
-    """Subset of the metadata for testing."""
-    metadata = tmp_path / "metadata.csv"
-    rows = []
-    for i in range(3):
-        filename = f"file_{i}.ogg"
-        primary_label = f"asbfly"
-        rows.append(
-            {
-                "primary_label": primary_label,
-                "filename": filename,
-            }
-        )
-        sr = 32_000
-        # create 2 channel audio from random noise
-        y = torch.tensor(np.random.rand(2 * 10 * sr), dtype=torch.float32).reshape(
-            2, -1
-        )
-        torchaudio.save(str(tmp_path / filename), y, sr)
-    df = pd.DataFrame(rows)
-    df.to_csv(metadata, index=False)
-    return metadata
-
-
-@pytest.fixture
-def soundscape_path(tmp_path):
-    """Subset of the metadata for testing."""
-    soundscape = tmp_path / "soundscape"
-    soundscape.mkdir()
-    for i in range(3):
-        filename = f"file_{i}.ogg"
-        sr = 32_000
-        # create 2 channel audio from random noise
-        y = torch.tensor(np.random.rand(2 * 10 * sr), dtype=torch.float32).reshape(
-            2, -1
-        )
-        torchaudio.save(str(soundscape / filename), y, sr)
-    return soundscape
 
 
 def test_google_vocalization_soundscape_dataset(metadata_path, soundscape_path):
@@ -56,7 +9,7 @@ def test_google_vocalization_soundscape_dataset(metadata_path, soundscape_path):
     row = next(iter(dataset))
     assert row.keys() == {"row_id", "embedding", "logits"}
     assert row["embedding"].shape == (1280,)
-    assert row["logits"].shape == (1,)
+    assert row["logits"].shape == (3,)
 
 
 def test_google_vocalization_soundscape_data_module(metadata_path, soundscape_path):
@@ -70,5 +23,5 @@ def test_google_vocalization_soundscape_data_module(metadata_path, soundscape_pa
     batch = next(data_module.predict_dataloader())
     assert batch.keys() == {"row_id", "embedding", "logits", "prediction"}
     assert batch["embedding"].shape == (4, 1280)
-    assert batch["logits"].shape == (4, 1)
-    assert batch["prediction"].shape == (4, 1)
+    assert batch["logits"].shape == (4, 3)
+    assert batch["prediction"].shape == (4, 3)
