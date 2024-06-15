23042ac2a530b0fcc3b3202f3078c0243c27e744
Murilo Gustineli <murilogustineli@gmail.com>
2024-06-15 21:23:41 +0000

    added TrainClassifier to birdnet workflow

--- birdclef/experiment/birdnet/data.py
+++ birdclef/experiment/birdnet/data.py
@@ -1,10 +1,17 @@
 import math
+import os
+import warnings
 from pathlib import Path
 
 import lightning as pl
 import torch
+from petastorm.spark import SparkDatasetConverter, make_spark_converter
+from pyspark.sql import functions as F
+from pyspark.sql.types import IntegerType
 from torch.utils.data import DataLoader, IterableDataset
+from torchvision.transforms import v2
 
+from birdclef.config import SPECIES
 from birdclef.inference.birdnet import BirdNetInference
 
 
@@ -93,3 +100,108 @@ class BirdNetSoundscapeDataModule(pl.LightningDataModule):
 
     def predict_dataloader(self):
         return self.dataloader
+
+
+# create a transform to convert a list of numbers into a sparse tensor
+class ToSparseTensor(v2.Transform):
+    def forward(self, batch):
+        if "label" in batch:
+            batch["label"] = batch["label"].to_sparse()
+        return batch
+
+
+class PetastormDataModule(pl.LightningDataModule):
+    def __init__(
+        self,
+        spark,
+        input_path,
+        label_col,
+        feature_col,
+        batch_size=64,
+        num_partitions=os.cpu_count(),
+        workers_count=os.cpu_count(),
+    ):
+        super().__init__()
+        cache_dir = "file:///mnt/data/tmp"
+        spark.conf.set(
+            SparkDatasetConverter.PARENT_CACHE_DIR_URL_CONF, Path(cache_dir).as_posix()
+        )
+        self.spark = spark
+        self.input_path = input_path
+        self.label_col = label_col
+        self.feature_col = feature_col
+        self.batch_size = batch_size
+        self.num_partitions = num_partitions
+        self.workers_count = workers_count
+
+    def _prepare_dataframe(self, df):
+        """Prepare the DataFrame for training by ensuring correct types and repartitioning"""
+        return (
+            df.withColumnRenamed(self.feature_col, "features")
+            .withColumnRenamed(self.label_col, "label")
+            .select(
+                F.col("features").cast("array<float>").alias("features"),
+                F.col("label").cast("array<float>").alias("label"),
+                F.col("species_index"),
+            )
+            .repartition(self.num_partitions)
+        )
+
+    def _train_valid_split(self, df):
+        """
+        Perform train/valid random split
+        :return: train_df, valid_df Spark DataFrames
+        """
+        train_df, valid_df = df.randomSplit([0.8, 0.2], seed=42)
+        train_df = self._prepare_dataframe(train_df)
+        valid_df = self._prepare_dataframe(valid_df)
+        return train_df.cache(), valid_df.cache()
+
+    def setup(self, stage=None):
+        """Setup dataframe for petastorm spark converter"""
+        with warnings.catch_warnings():
+            warnings.simplefilter(action="ignore", category=FutureWarning)
+
+            # UDF to retrieve species index
+            def species_index(name):
+                species = Path(name).parent.name
+                species_idx = SPECIES.index(species)
+                return species_idx
+
+            # register the UDF
+            species_index_udf = F.udf(species_index, IntegerType())
+
+            # read data
+            df = self.spark.read.parquet(self.input_path).cache()
+            df_species = df.withColumn("species_index", species_index_udf("name"))
+
+            # train/valid Split
+            self.train_data, self.valid_data = self._train_valid_split(df=df_species)
+            print(
+                f"\ntrain count: {self.train_data.count()}\n"
+                f"valid count: {self.valid_data.count()}\n",
+            )
+
+            # setup petastorm data conversion from Spark to PyTorch
+            self.converter_train = make_spark_converter(self.train_data)
+            self.converter_valid = make_spark_converter(self.valid_data)
+
+    def _dataloader(self, converter):
+        transform = v2.Compose([ToSparseTensor()])
+        with warnings.catch_warnings():
+            warnings.simplefilter(action="ignore", category=FutureWarning)
+            with converter.make_torch_dataloader(
+                batch_size=self.batch_size,
+                num_epochs=1,
+                workers_count=self.workers_count,
+            ) as dataloader:
+                for batch in dataloader:
+                    yield transform(batch)
+
+    def train_dataloader(self):
+        for batch in self._dataloader(self.converter_train):
+            yield batch
+
+    def val_dataloader(self):
+        for batch in self._dataloader(self.converter_valid):
+            yield batch
--- birdclef/experiment/birdnet/workflow.py
+++ birdclef/experiment/birdnet/workflow.py
@@ -1,8 +1,22 @@
+import itertools
+import os
 from argparse import ArgumentParser
+from pathlib import Path
 
+import lightning as pl
 import luigi
+import luigi.contrib.gcs
 import pandas as pd
+import torch
+import wandb
+from lightning.pytorch.callbacks import LearningRateFinder
+from lightning.pytorch.callbacks.early_stopping import EarlyStopping
+from lightning.pytorch.callbacks.model_checkpoint import ModelCheckpoint
+from lightning.pytorch.loggers import WandbLogger
 
+from birdclef.experiment.birdnet.data import PetastormDataModule
+from birdclef.experiment.google_vocalization.data import PetastormDataModule
+from birdclef.experiment.model import LinearClassifier, TwoLayerClassifier
 from birdclef.inference.birdnet import BirdNetInference
 from birdclef.tasks import RsyncGCSFiles, maybe_gcs_target
 from birdclef.utils import spark_resource
@@ -81,14 +95,141 @@ class EmbedWorkflow(luigi.Task):
             )
 
 
+class TrainClassifier(luigi.Task):
+    input_path = luigi.Parameter()
+    default_model_dir = luigi.Parameter()
+    label_col = luigi.Parameter()
+    feature_col = luigi.Parameter()
+    loss = luigi.Parameter()
+    model = luigi.Parameter()
+    hidden_layer_size = luigi.OptionalIntParameter(default=64)
+    hyper_params = luigi.OptionalDictParameter(default={})
+    species_label = luigi.OptionalBoolParameter(default=False)
+    batch_size = luigi.IntParameter(default=1000)
+    num_partitions = luigi.IntParameter(default=os.cpu_count())
+    two_layer = luigi.OptionalBoolParameter(default=False)
+
+    def output(self):
+        # save the model run
+        return luigi.contrib.gcs.GCSTarget(f"{self.default_model_dir}/_SUCCESS")
+
+    def run(self):
+        # Hyperparameters
+        hp = HyperparameterGrid()
+        model_params, loss_params, _ = hp.get_hyperparameter_config()
+        # get model and loss objects
+        torch_model = model_params[self.model]
+
+        with spark_resource() as spark:
+            # data module
+            data_module = PetastormDataModule(
+                spark,
+                self.input_path,
+                self.label_col,
+                self.feature_col,
+                self.batch_size,
+                self.num_partitions,
+            )
+            data_module.setup()
+
+            # get parameters for the model
+            num_features = int(
+                len(data_module.train_data.select("features").first()["features"])
+            )
+            num_labels = int(
+                len(data_module.train_data.select("label").first()["label"])
+            )
+
+            # model module
+            if self.two_layer:
+                model = torch_model(
+                    num_features,
+                    num_labels,
+                    loss=self.loss,
+                    hidden_layer_size=self.hidden_layer_size,
+                    hp_kwargs=self.hyper_params,
+                    species_label=self.species_label,
+                )
+            else:
+                model = torch_model(
+                    num_features,
+                    num_labels,
+                    loss=self.loss,
+                    species_label=self.species_label,
+                )
+
+            # initialise the wandb logger and name your wandb project
+            print(f"\nwanb name: {Path(self.default_model_dir).name}")
+            print(f"wanb save dir: {self.default_model_dir}\n")
+            wandb_logger = WandbLogger(
+                project="birdclef-2024",
+                name=Path(self.default_model_dir).name,
+                save_dir=self.default_model_dir,
+            )
+
+            # add your batch size to the wandb config
+            wandb_logger.experiment.config["batch_size"] = self.batch_size
+
+            model_checkpoint = ModelCheckpoint(
+                dirpath=os.path.join(self.default_model_dir, "checkpoints"),
+                monitor="val_loss",
+                save_last=True,
+            )
+
+            # trainer
+            trainer = pl.Trainer(
+                max_epochs=20,
+                accelerator="gpu" if torch.cuda.is_available() else "cpu",
+                reload_dataloaders_every_n_epochs=1,
+                default_model_dir=self.default_model_dir,
+                logger=wandb_logger,
+                callbacks=[
+                    EarlyStopping(monitor="val_auroc", mode="max"),
+                    model_checkpoint,
+                    LearningRateFinder(),
+                ],
+            )
+            trainer.fit(model, data_module)
+
+            # finish W&B
+            wandb.finish()
+
+        # write the output
+        with self.output().open("w") as f:
+            f.write("")
+
+
+class HyperparameterGrid:
+    def get_hyperparameter_config(self):
+        # Model and Loss mappings
+        model_params = {
+            "linear": LinearClassifier,
+            "two_layer": TwoLayerClassifier,
+        }
+        loss_params = {
+            "bce": {},
+            "asl": {
+                "gamma_neg": [0, 2, 4],
+                "gamma_pos": [0, 1],
+            },
+            "sigmoidf1": {
+                "S": [-1, -15, -30],
+                "E": [0, 1, 2],
+            },
+        }
+        hidden_layers = [64, 128, 256]
+        return model_params, loss_params, hidden_layers
+
+
 class Workflow(luigi.Task):
     remote_root = luigi.Parameter()
     local_root = luigi.Parameter()
-
     audio_path = luigi.Parameter()
     metadata_path = luigi.Parameter()
     intermediate_path = luigi.Parameter()
     output_path = luigi.Parameter()
+    input_path = luigi.Parameter()
+    default_model_dir = luigi.Parameter()
 
     def run(self):
         yield EmbedWorkflow(
@@ -101,12 +242,58 @@ class Workflow(luigi.Task):
         )
 
         # TODO: train workflow
+        label_col, feature_col = "logits", "embedding"
+        # get hyperparameter config
+        hp = HyperparameterGrid()
+        _, loss_params, hidden_layers = hp.get_hyperparameter_config()
+
+        # Linear model grid search
+        model, species_label = "linear", False
+        for loss in loss_params:
+            default_dir = f"{self.default_model_dir}-{model}-{loss}"
+            if species_label:
+                default_dir = f"{self.default_model_dir}-{model}-{loss}-species-label"
+            yield TrainClassifier(
+                input_path=self.input_path,
+                default_model_dir=default_dir,
+                label_col=label_col,
+                feature_col=feature_col,
+                loss=loss,
+                model=model,
+                species_label=species_label,
+            )
+
+        # # TwoLayer model grid search
+        # model, hidden_layer_size, species_label = "two_layer", 256, True
+        # for loss in loss_params:
+        #     for hp_params in self.generate_loss_hp_params(loss_params[loss]):
+        #         param_log = [f"{k}{v}" for k, v in hp_params.items()]
+        #         if len(param_log) > 0:
+        #             param_name = "-".join(param_log)
+        #             default_dir = f"{self.default_model_dir}-twolayer-{loss}-{param_name}-hidden{hidden_layer_size}"
+        #             if species_label:
+        #                 default_dir = f"{default_dir}-species-label"
+        #             yield TrainClassifier(
+        #                 input_path=self.input_path,
+        #                 default_model_dir=default_dir,
+        #                 label_col=label_col,
+        #                 feature_col=feature_col,
+        #                 loss=loss,
+        #                 model=model,
+        #                 hidden_layer_size=hidden_layer_size,
+        #                 hyper_params=hp_params,
+        #                 species_label=species_label,
+        #                 two_layer=True,
+        #             )
 
 
 def parse_args():
     parser = ArgumentParser()
 
     default_out_folder = "birdnet"
+    gcs_root_path = "gs://dsgt-clef-birdclef-2024"
+    train_data_path = f"data/processed/{default_out_folder}/v1"
+    model_dir_path = f"models/torch-v1-{default_out_folder}"
     defaults = {
         "remote-root": "gs://dsgt-clef-birdclef-2024/data",
         "local-root": "/mnt/data",
@@ -116,6 +303,8 @@ def parse_args():
         "output-path": f"processed/{default_out_folder}/v1",
         "scheduler-host": "services.us-central1-a.c.dsgt-clef-2024.internal",
         "workers": 2,
+        "input_path": f"{gcs_root_path}/{train_data_path}",
+        "default_model_dir": f"{gcs_root_path}/{model_dir_path}",
     }
 
     for arg, default in defaults.items():
